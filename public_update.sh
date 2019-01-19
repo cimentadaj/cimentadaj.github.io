d190ec8cfe75eba7dbf07e7fecb7f9f3226f8147
cimentadaj <cimentadaj>
2019-01-19 14:33:15 +0100

    Adds comments in public bash file

--- public_update.sh
+++ public_update.sh
@@ -8,6 +8,8 @@ echo Using R executable at /c/Program\ Files/R/R-3.5.1/bin/R.exe. Replace if the
 # Change this when computer changes
 /c/Program\ Files/R/R-3.5.1/bin/R.exe -e "blogdown::hugo_build()"
 
+
+echo Git pull and commit in root
 git pull
 
 git add .
@@ -16,28 +18,34 @@ git commit -m 'New blog post'
 
 git push
 
+echo Checkout to master and pull
 git checkout master
 
 git pull
 
 cd ..
 
+echo Clone repo to temp_folder and remove old folder
 git clone -b root https://github.com/cimentadaj/cimentadaj.github.io.git tempfolder
 
 rm -R cimentadaj.github.io/*
 
 cd tempfolder
 
+echo Copy public folder
 cp -R public/. ../cimentadaj.github.io/
 
 cd ../cimentadaj.github.io
 
+echo Remove temporary folder
 rm -rf ../tempfolder
 
+echo Add new changes in master
 git add .
 
 git commit -m 'New blog post'
 
 git push
 
+echo Checkout to root
 git checkout root
